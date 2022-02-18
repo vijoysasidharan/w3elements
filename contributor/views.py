3842c4beeec3f5ae6e87f215fe9f4347c2859b09
vijoy sasidharan <devuinfosys@gmail.com>
2022-02-18 15:45:52 +0530

    subdomain configured

--- contributor/views.py
+++ contributor/views.py
@@ -1,3 +1,5 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "contributor/index.html")
---
+++ w3elements/hosts.py
@@ -0,0 +1,7 @@
+from django_hosts import patterns, host
+
+host_patterns = patterns(
+  '',
+    host(r'', 'w3elements.urls', name=' '),
+    host(r'contributor', 'contributor.urls', name='contributor'),
+)
