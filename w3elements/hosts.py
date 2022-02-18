6913a40ac7707e4b328bcfc2fe18cff268ad4976
vijoy sasidharan <devuinfosys@gmail.com>
2022-02-18 17:51:36 +0530

    simple updates

--- w3elements/hosts.py
+++ w3elements/hosts.py
@@ -3,6 +3,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
   '',
+    host(r'', 'w3elements.urls', name=''),
     host(r'www', settings.ROOT_URLCONF, name='www'),
-    host(r'(\w+)', 'contributor.urls', name='wildcard'),
+    host(r'contributor', 'contributor.urls', name='contributor'),
 )
